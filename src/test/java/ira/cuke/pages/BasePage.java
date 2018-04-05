58b016bd11ceba53ac7714fee8a9bb20fcff5ab3
Irina Gruzdeva <igruzdeva@intermedia.net>
2018-04-05 23:26:39 +0300

    findElements and scrollIntoView methods

--- src/test/java/ira/cuke/pages/BasePage.java
+++ src/test/java/ira/cuke/pages/BasePage.java
@@ -2,8 +2,11 @@ package ira.cuke.pages;
 
 import ira.cuke.DriverUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class BasePage {
 
@@ -14,6 +17,18 @@ public class BasePage {
     }
 
     public WebElement findElement(By str) {
-        return driver.findElement(str);
+        WebElement element = driver.findElement(str);
+        scrollIntoView(element);
+        return element;
     }
+
+    public List<WebElement> findElements(By str) {
+        return driver.findElements(str);
+    }
+
+    public void scrollIntoView(WebElement element) {
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
 }
